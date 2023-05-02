4bfa4ee428478c22bb7457265121bdf7b8a4ba75
HopeNine <125716406+HopeNine@users.noreply.github.com>
2023-05-02 22:39:10 +0500

    Update animal_class.py

--- HW_21_7_1/animal_class.py
+++ HW_21_7_1/animal_class.py
@@ -1,3 +1,5 @@
+#создаем класс Pat
+
 class Pet:
 
     def __init__(self, animal="", name="", gender="", age<=0):
@@ -8,6 +10,20 @@ class Pet:
 
     def init_from_dict(self, cats_dict):
         self.animal = cats_dict.get("animal")
+        self.name = cats_dict.get("name")
+        self.gender = cats_dict.get("gender")
+        self.age = cats_dict.get("age")
+
+#создаем класс Cat
+
+class Cat:
+
+    def __init__(self, name="", gender="", age=0):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def init_from_dict(self, cats_dict):
         self.name = cats_dict.get("name")
         self.gender = cats_dict.get("gender")
         self.age = cats_dict.get("age")
